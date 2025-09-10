c4776b29dee2d8329cd5ac0b7085bcd48ea17b84
galihnur9 <galihnurr9@gmail.com>
2025-09-10 09:43:34 +0700

    Update 2 agar bisa deploy di PWS

--- adidos/settings.py
+++ adidos/settings.py
@@ -31,7 +31,6 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "galih-nur41-adidos.pbp.cs.ui.ac.id"]
 
 
