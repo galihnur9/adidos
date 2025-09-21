48b3d529c39d837079b77e8628780d440fe7a489
galihnur9 <galihnurr9@gmail.com>
2025-09-21 21:18:34 +0700

    Done Tugas 3

--- adidos/settings.py
+++ adidos/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "galih-nur41-adidos.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://galih-nur41-adidos.pbp.cs.ui.ac.id/"
+    "https://galih-nur41-adidos.pbp.cs.ui.ac.id"
 ]
 
 
